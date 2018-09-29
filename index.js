be64cb2e53d38d65e2322324c8a244c13c8809c0
Wesley Wang <wesleywang2@gmail.com>
2018-09-28 19:24:49 -0700

    Add html, css, and js files with basic arithmetic functions and buttons

---
+++ index.js
@@ -0,0 +1,19 @@
+function add(n, m){
+  return n + m;
+}
+
+function subtract(n, m){
+  return n - m;
+}
+
+function multiply(n, m){
+  return n * m;
+}
+
+function divide(n, m){
+  return n / m;
+}
+
+function operate(operation, n, m){
+  return operation(n, m);
+}
