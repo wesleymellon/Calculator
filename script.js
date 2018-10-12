6b904024923186912d87a04d9fdd9500d7bf4855
Wesley Wang <wesleywang2@gmail.com>
2018-10-11 18:49:03 -0700

    Calculate function works with multiplication and division, but not addition and subtraction. Also does not update the expression array

--- script.js
+++ script.js
@@ -27,7 +27,6 @@ let enter = document.getElementById("enter");
 
 Array.from(numbers).forEach(function(element){
   element.addEventListener("click", function(e){
-    console.log(e.srcElement.innerHTML);
     expressionArray.push(e.srcElement.innerHTML);
     display.innerHTML = expressionArray.join(" ");
   });
@@ -35,7 +34,6 @@ Array.from(numbers).forEach(function(element){
 
 Array.from(operators).forEach(function(element){
   element.addEventListener("click", function(e){
-    console.log(e.srcElement.innerHTML);
     expressionArray.push(e.srcElement.innerHTML);
     display.innerHTML = expressionArray.join(" ");
   });
@@ -46,14 +44,83 @@ allClear.addEventListener("click", function(e){
   display.innerHTML = "0";
 });
 
-// enter.addEventListener("click", function(e){
-//   expressionArray.reduce
-// });
+let testArrayPlus = ["5", "+", "6", "*", "3", "/", "2", "0"];
+let testArray = ["5", "6", "*", "3", "/", "2", "0"];
 
-function foo(origArray){
-  let numbersArray = [];
-  let operatorsArray = [];
-  origArray.forEach(function(e){
-  });
+function calculate(origArray){
+  let numbersArray = origArray.join("").split(/\D+/);
+  let operatorsArray = origArray.join("").split(/\d+/);
+  let numbersLength = numbersArray.length;
+  let operatorsLength = operatorsArray.length;
+
+  console.log("before the loop begins");
+  console.log(numbersArray);
+  console.log(operatorsArray);
+
+  //Multiplication and Division calculations
+  for(i = 1; i < operatorsLength - 1; i++){
+    let position = 1;
+
+    console.log(`i = ${i}`);
+    console.log(`position = ${position}`);
+
+    if(operatorsArray[position] == "*"){
+
+      console.log(`Found a * position = ${position} and just entered the if statement`);
+
+      console.log(`numberssArray[position - 1] = ${numbersArray[position - 1]}`)
+      console.log(`numbersArray[position] = ${numbersArray[position]}`)
+
+      let newElement = multiply(numbersArray[position - 1], numbersArray[position]);
+
+      console.log(` the newElement is equal to ${newElement}`);
+
+      operatorsArray = exciseElement(operatorsArray, position);
+      numbersArray = exciseElement(numbersArray, position - 1);
+      numbersArray = exciseElement(numbersArray, position - 1);
+
+      numbersArray = numbersArray.slice(0, position - 1).concat(newElement).concat(numbersArray.slice(position - 1));
+
+      position--;
+    }
+
+    else if(operatorsArray[position] == "/"){
+
+      console.log(`Found a / position = ${position} and just entered the else if statement`);
+
+      let newElement = divide(numbersArray[position - 1], numbersArray[position]);
+
+      operatorsArray = exciseElement(operatorsArray, position);
+      numbersArray = exciseElement(numbersArray, position - 1);
+      numbersArray = exciseElement(numbersArray, position - 1);
+
+      numbersArray = numbersArray.slice(0, position - 1).concat(newElement).concat(numbersArray.slice(position - 1));
+
+      position--;
+    };
+
+    position++;
+
+    console.log(`end of loop iteration i = ${i}`);
+    console.log(`position = ${position}`);
+    console.log(numbersArray);
+    console.log(operatorsArray);
+  };
+
+  console.log(`end of the function`);
+  console.dir(`numbersArray = ${numbersArray}`);
+  console.log(`operatorsArray = ${operatorsArray}`);
+
 };
 
+function exciseElement(origArray, index){
+  return origArray.slice(0, index).concat(origArray.slice(index + 1));
+};
+
+function isMultOrDiv(str){
+  return /\*|\//.test(str);
+}
+
+function isAddOrSub(str){
+  return /\+|\-/.test(str);
+}
