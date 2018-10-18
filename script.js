93f9b51ddc980d550398edcbac9fd3278c6a241f
Wesley Wang <wesleywang2@gmail.com>
2018-10-17 20:09:09 -0700

    Add decimal operator and remove console log statements

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function add(n, m){
 }
 
 function subtract(n, m){
-  return n - m;
+  return Number(n) - Number(m);
 }
 
 function multiply(n, m){
-  return n * m;
+  return Number(n) * Number(m);
 }
 
 function divide(n, m){
-  return n / m;
+  return Number(n) / Number(m);
 }
 
 function operate(operation, n, m){
@@ -21,9 +21,11 @@ function operate(operation, n, m){
 let expressionArray = [];
 let numbers = document.getElementsByClassName("number");
 let display = document.getElementById("display");
-let allClear = document.getElementById("clear");
+let allClear = document.getElementById("allClear");
 let operators = document.getElementsByClassName("operator");
 let equal = document.getElementById("equal");
+let clear = document.getElementById("clear");
+let decimal = document.getElementById("decimal");
 
 Array.from(numbers).forEach(function(element){
   element.addEventListener("click", function(e){
@@ -34,16 +36,22 @@ Array.from(numbers).forEach(function(element){
 
 Array.from(operators).forEach(function(element){
   element.addEventListener("click", function(e){
-    console.log(expressionArray[expressionArray.length - 1]);
     if(!(expressionArray[0] == undefined) && isNaN(expressionArray[expressionArray.length - 1])){
-      console.log("popping!");
-      console.log(`expression array before pop ${expressionArray}`);
       expressionArray.pop();
-      console.log(`expression array after pop ${expressionArray}`);
     };
     expressionArray.push(e.srcElement.innerHTML);
     display.innerHTML = expressionArray.join("");
   });
+});
+
+clear.addEventListener("click", function(e){
+  expressionArray.pop();
+  if(expressionArray[0] == undefined){
+    display.innerHTML = "0";
+  }
+  else{
+    display.innerHTML = expressionArray.join("");
+  }
 });
 
 allClear.addEventListener("click", function(e){
@@ -63,10 +71,6 @@ equal.addEventListener("click", function(e){
   };
 });
 
-let testArrayPlus = ["5", "+", "6.5", "*", "3", "/", "2", "0"];
-let testArray = ["5", "6", "*", "3", "/", "2", "0"];
-let testExpression = ["5", "6", ".", "5", "*", "3", "/", "2", "0"]
-
 function calculate(origArray){
   // let numbersArray = origArray.join("").split(/\D+/);
   // let operatorsArray = origArray.join("").split(/\d+/);
@@ -75,37 +79,27 @@ function calculate(origArray){
   let numbersLength = numbersArray.length;
   let operatorsLength = operatorsArray.length;
   let position = 1;
+  let isNegative = false;
 
-  //First Check if the first and last elements of the origArray are numbers or .
-  console.log(`origArray[0] = ${origArray[0]}`);
-  console.log(`origArray[last] = ${origArray[origArray.length - 1]}`);
-  if(/[^0-9\.]+/.test(origArray[0]) || /[^0-9\.]+/.test(origArray[origArray.length - 1])){
+  if(/\-/.test(origArray[0])){
+    isNegative = true;
+    if(typeof origArray[0] == "number"){
+      origArray[0] == Math.abs(origArray[0]);
+    }
+    else{
+      origArray.shift();
+    };
+  }
+
+  if(/[^0-9\.\-]+/.test(origArray[0]) || /[^0-9\.]+/.test(origArray[origArray.length - 1])){
     alert("Please Enter a Valid Mathematical Expression");
-    console.log(expressionArray);
     return undefined;
   }
-  //Second Check through the array to see if there is any division by 0
-
-  console.log("before the loop begins");
-  console.log(numbersArray);
-  console.log(operatorsArray);
 
   //Multiplication and Division calculations
   for(i = 1; i < operatorsLength - 1; i++){
-
-    console.log(`i = ${i}`);
-    console.log(`position = ${position}`);
-
     if(operatorsArray[position] == "*"){
-
-      console.log(`Found a * position = ${position} and just entered the if statement`);
-
-      console.log(`numberssArray[position - 1] = ${numbersArray[position - 1]}`)
-      console.log(`numbersArray[position] = ${numbersArray[position]}`)
-
       let newElement = multiply(numbersArray[position - 1], numbersArray[position]);
-
-      console.log(` the newElement is equal to ${newElement}`);
 
       operatorsArray = exciseElement(operatorsArray, position);
       numbersArray = exciseElement(numbersArray, position - 1);
@@ -117,9 +111,6 @@ function calculate(origArray){
     }
 
     else if(operatorsArray[position] == "/"){
-
-      console.log(`Found a / position = ${position} and just entered the else if statement`);
-
       let newElement = divide(numbersArray[position - 1], numbersArray[position]);
 
       operatorsArray = exciseElement(operatorsArray, position);
@@ -132,11 +123,6 @@ function calculate(origArray){
     };
 
     position++;
-
-    console.log(`end of loop iteration i = ${i}`);
-    console.log(`position = ${position}`);
-    console.log(numbersArray);
-    console.log(operatorsArray);
   };
 
   //Addition and Subtraction calculations
@@ -164,10 +150,6 @@ function calculate(origArray){
       numbersArray = numbersArray.slice(0, position - 1).concat(newElement).concat(numbersArray.slice(position - 1));
     }
   };
-
-  console.log(`end of the function`);
-  console.log(`numbersArray = ${numbersArray}`);
-  console.log(`operatorsArray = ${operatorsArray}`);
   return numbersArray[0];
 
 };
